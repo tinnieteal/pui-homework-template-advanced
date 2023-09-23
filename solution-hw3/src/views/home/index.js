df3c6c063c7ea22b6842125be4eaf371d1aa5810
tinnieteal <61638345+tinnieteal@users.noreply.github.com>
2023-09-22 23:01:56 -0500

    fix path of image

--- solution-hw3/src/views/home/index.js
+++ solution-hw3/src/views/home/index.js
@@ -11,22 +11,22 @@ function Homepage() {
       <main>
         <div className="grid-container">
               {/* cinnamon roll #1 */}
-              <Item title="Original cinnamon roll" img="/assets/products/original-cinnamon-roll.jpg" price="$ 2.49"/>
+              <Item title="Original cinnamon roll" img="assets/products/original-cinnamon-roll.jpg" price="$ 2.49"/>
                 
               {/* cinnamon roll #2 */}
-              <Item title="Apple cinnamon roll" img="/assets/products/apple-cinnamon-roll.jpg" price="$ 3.49"/>
+              <Item title="Apple cinnamon roll" img="assets/products/apple-cinnamon-roll.jpg" price="$ 3.49"/>
 
               {/* cinnamon roll #3 */}
-              <Item title="Raisin cinnamon roll" img="/assets/products/raisin-cinnamon-roll.jpg" price="$ 2.99"/>
+              <Item title="Raisin cinnamon roll" img="assets/products/raisin-cinnamon-roll.jpg" price="$ 2.99"/>
 
               {/* cinnamon roll #4 */}
-              <Item title="Walnut cinnamon roll" img="/assets/products/walnut-cinnamon-roll.jpg" price="$ 3.49"/>
+              <Item title="Walnut cinnamon roll" img="assets/products/walnut-cinnamon-roll.jpg" price="$ 3.49"/>
 
               {/* cinnamon roll #5 */}
-              <Item title="Double-chocolate cinnamon roll" img="/assets/products/double-chocolate-cinnamon-roll.jpg" price="$ 3.99"/>
+              <Item title="Double-chocolate cinnamon roll" img="assets/products/double-chocolate-cinnamon-roll.jpg" price="$ 3.99"/>
 
               {/* cinnamon roll #6 */}
-              <Item title="Strawberry cinnamon roll" img="/assets/products/strawberry-cinnamon-roll.jpg" price="$ 3.99"/>
+              <Item title="Strawberry cinnamon roll" img="assets/products/strawberry-cinnamon-roll.jpg" price="$ 3.99"/>
 
         </div>
     </main>      
