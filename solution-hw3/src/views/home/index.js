4e2e9fdd6d9eac87ce39a3f0e9131d70c7aba24c
tinnieteal <61638345+tinnieteal@users.noreply.github.com>
2023-09-23 21:51:24 -0500

    updated file structure + incorporated JSX

--- solution-hw3/src/views/home/index.js
+++ solution-hw3/src/views/home/index.js
@@ -1,5 +1,43 @@
-import NavBar from './navbar.js'; 
-import Item from './item.js';
+import NavBar from '../components/navbar.js'; 
+import Item from '../components/item.js';
+
+const cinnamonRolls = [
+  {
+    title: "Original cinnamon roll",
+    img: "assets/products/original-cinnamon-roll.jpg",
+    price: "$ 2.49"
+  },
+  {
+    title: "Apple cinnamon roll",
+    img: "assets/products/apple-cinnamon-roll.jpg",
+    price: "$ 3.49"
+  },
+  {
+    title: "Raisin cinnamon roll",
+    img: "assets/products/raisin-cinnamon-roll.jpg",
+    price: "$ 2.99"
+  },
+  {
+    title: "Walnut cinnamon roll",
+    img: "assets/products/walnut-cinnamon-roll.jpg",
+    price: "$ 3.49"
+  },
+  {
+    title: "Double-chocolate cinnamon roll",
+    img: "assets/products/double-chocolate-cinnamon-roll.jpg",
+    price: "$ 3.99"
+  },
+  {
+    title: "Strawberry cinnamon roll",
+    img: "assets/products/strawberry-cinnamon-roll.jpg",
+    price: "$ 3.99"
+  }
+];
+
+// create 6 cinammon rolls using JSX
+const itemList = cinnamonRolls.map((item, index) => (
+  <Item key={index} title={item.title} img={item.img} price={item.price} />
+));
 
 function Homepage() {
   return (
@@ -10,24 +48,7 @@ function Homepage() {
 
       <main>
         <div className="grid-container">
-              {/* cinnamon roll #1 */}
-              <Item title="Original cinnamon roll" img="assets/products/original-cinnamon-roll.jpg" price="$ 2.49"/>
-                
-              {/* cinnamon roll #2 */}
-              <Item title="Apple cinnamon roll" img="assets/products/apple-cinnamon-roll.jpg" price="$ 3.49"/>
-
-              {/* cinnamon roll #3 */}
-              <Item title="Raisin cinnamon roll" img="assets/products/raisin-cinnamon-roll.jpg" price="$ 2.99"/>
-
-              {/* cinnamon roll #4 */}
-              <Item title="Walnut cinnamon roll" img="assets/products/walnut-cinnamon-roll.jpg" price="$ 3.49"/>
-
-              {/* cinnamon roll #5 */}
-              <Item title="Double-chocolate cinnamon roll" img="assets/products/double-chocolate-cinnamon-roll.jpg" price="$ 3.99"/>
-
-              {/* cinnamon roll #6 */}
-              <Item title="Strawberry cinnamon roll" img="assets/products/strawberry-cinnamon-roll.jpg" price="$ 3.99"/>
-
+              {itemList}
         </div>
     </main>      
     </div>
